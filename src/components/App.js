de32ba33cf117803bb66ba239e74585db505562b
Gourav Sethi <61932647+gouravsethi933@users.noreply.github.com>
2022-02-22 17:09:16 +0530

    string

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,47 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
-const App = () => {
+const App = ({ slides }) => {
+  const [index, setIndex] = useState(0);
+
+  const handlePrev = () => {
+    if (index === 0) {
+      return;
+    }
+    setIndex(index - 1);
+  };
+
+  const handleNext = () => {
+    if (index === slides.length - 1) {
+      return;
+    }
+    setIndex(index + 1);
+  };
+  const handleRestart = () => {
+    setIndex(0);
+  };
   return (
-    <></>
-  )
-}
-
+    <>
+      <div className="App">
+        <h1 data-testid="title">{slides[index].tittle}</h1>
+        <p data-testid="text">{slides[index].text}</p>
+        <button type="btn" disabled data-testid="btn-prev" onClick={handlePrev}>
+          Previous
+        </button>
+        <button
+          type="btn"
+          disabled
+          data-testid="btn-restart"
+          onClick={handleRestart}
+        >
+          Restart
+        </button>
+        <button type="btn" disabled data-testid="btn-next" onClick={handleNext}>
+          Next
+        </button>
+      </div>
+    </>
+  );
+};
 
 export default App;
